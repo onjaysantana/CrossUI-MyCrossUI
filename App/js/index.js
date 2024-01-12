5cd627c93dbf0b7145754025f788a28cf3956687
onjaysantana <111399274+onjaysantana@users.noreply.github.com>
2024-01-12 12:08:24 -0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,39 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput3")
+                .setLeft("3.8095238095238093em")
+                .setTop("6.857142857142857em")
+                .setWidth("18em")
+                .setHeight("1.7523809523809524em")
+                .setLabelSize("8em")
+                .setLabelCaption("Date Picker")
+                .setType("date")
+            );
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel4")
+                .setDock("none")
+                .setLeft("6.857142857142857em")
+                .setTop("13.714285714285714em")
+                .setWidth("18em")
+                .setHeight("18em")
+                .setCaption("Foldable Panel")
+                .setToggleBtn(true)
+                .setCloseBtn(true)
+                .setRefreshBtn(true)
+                .setCustomStyle({
+                    "KEY" : {
+                        "color" : "#B22222",
+                        "background-color" : "#FFFAF0"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
